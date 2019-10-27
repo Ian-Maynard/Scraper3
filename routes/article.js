8cca78d4942ddca892ddb388176f9c784194580c
Ian-Maynard <maynard.ian@gmail.com>
2019-10-27 19:21:43 -0400

    Multiple npm installations and minor changes

--- routes/article.js
+++ routes/article.js
@@ -1,3 +1,7 @@
+/* jshint esversion: 6 */ 
+/* jshint esversion: 8 */ 
+
+
 const express = require("express");
 // var query = require("../controllers/controller")
 var cheerio = require("cheerio");
@@ -7,7 +11,7 @@ var bitly = new Bitly('fd0a57a9269bf1d523ec4bd38c18f0812c444f04'); // Shorten UR
 var router = express.Router();
 
 function scraper(sURL,cr1,cr2) {
-var time = [];
+var time = []; // Array of articles and Notes for display
 request (sURL, function(error, response, html) { 
   var $ = cheerio.load(html);
   $(".rail-article-title").each(function(i, element)  {
